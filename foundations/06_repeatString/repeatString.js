049338030ade6a8941d8b69a6fdf186b21491d38
DevScissors <sean.scissors@gmail.com>
2026-04-13 14:03:08 -0700

    Javascript Exercises, repeatString completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return console.log("ERROR");
+    }
+    let repeatedString = '';
+    for (let i = 0; i < num; i++) {
+        repeatedString += string;
+    }
+    console.log(repeatedString);
 };
 
 // Do not edit below this line
