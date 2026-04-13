ae66b4e397d7d92bb92b8329db1f794bc76c710f
DevScissors <sean.scissors@gmail.com>
2026-04-13 14:19:17 -0700

    Javascript exercises, reverseString completed using a for loop

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let reversedString = '';
+    const splitString = str.split('');
+    for (let i = splitString.length - 1; i >= 0; i--) {
+        reversedString += splitString[i];
+    }
+    console.log(reversedString);
 };
+
+reverseString("This is a test");
 
 // Do not edit below this line
 module.exports = reverseString;
