f3dc483e858f59c3b84692db9b2da93d28bb6dbc
DevScissors <sean.scissors@gmail.com>
2026-04-13 17:49:36 -0700

    Javascript exercises, tempConversion passing all tests

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const tempToCelcius = (fahrenheit - 32) / (9 / 5);
+  return Math.round(tempToCelcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  const tempToFarenheit = (celcius * 9 / 5) + 32;
+  return Math.round(tempToFarenheit * 10) / 10;
 };
 
 // Do not edit below this line
