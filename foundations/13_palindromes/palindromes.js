3f19d4d9bda25860c48d4124221a6982386a4c4c
DevScissors <sean.scissors@gmail.com>
2026-04-20 21:22:00 -0700

    Javascript exercise, check if palindrome, tests passed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // 1. Convert to lowercase and remove non-alphanumeric characters
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    // 2. Perform the check (using simple reversal here)
+    return cleanStr === cleanStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
