53565bde3a5a781d5cb7153f1658312a85aeff75
DevScissors <sean.scissors@gmail.com>
2026-04-21 10:34:29 -0700

    Javascript exercises, fobonacci solved using recursion

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (fibNum) {
+    if (fibNum < 0) {
+        return "OOPS";
+    }
 
+    if (fibNum <= 1) {
+        return parseInt(fibNum);
+    }
+
+    return parseInt(fibonacci(fibNum - 1) + fibonacci(fibNum - 2));
 };
 
 // Do not edit below this line
