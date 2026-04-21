95503bea49e207fa834d6ae7ac16f3fe3c43e445
DevScissors <sean.scissors@gmail.com>
2026-04-21 11:14:44 -0700

    Javascript exercises, book titles received using map on the object

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let bookTitles = books.map(book => book.title)
+    return bookTitles;
 };
+
+getTheTitles([
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+])
 
 // Do not edit below this line
 module.exports = getTheTitles;
